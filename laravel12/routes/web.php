293d8eb00c629078587256f990e910d329a84a4f
Firda <firdarahayu105@gmail.com>
2026-03-08 13:10:31 +0700

    update route

--- laravel12/routes/web.php
+++ laravel12/routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ProfileController::class, 'index'])->name('home');
 
 Route::get('/about', [ProfileController::class, 'about'])->name('about.page');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact.page');
